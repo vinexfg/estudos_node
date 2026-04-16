eb480ee5b17b6c2177de43e953abb97cb2abc8cd
vinexfg <vinysilvaa20@gmail.com>
2026-04-16 11:24:26 -0300

    a

--- capitulo4/index.js
+++ capitulo4/index.js
@@ -1,43 +1,139 @@
 import bcrypt from "bcrypt"
-import { log } from "node:console"
-import promptMOdule from "prompt-sync"
+import promptModule from "prompt-sync"
+import { MongoClient } from "mongodb"
 
 
 
+const dbUrl = 'mongodb://localhost:27017'
+const client = new MongoClient(dbUrl)
+const prompt = promptModule()
+
+let hasPasswords = false
+let passwordsCollection, authCollection
+const dbName = "passwordManeger"
 
 
-const prompt = promptMOdule()
+const main = async () => {
+    try {
+        await client.connect()
+        console.log('connect sucessfully to server');
+        const db = client.db(dbName)
+        authCollection = db.collection("auth")
+        passwordsCollection = db.collection("password")
+        const hashedPassword = await authCollection.findOne({ type: "auth" })
+        hasPasswords = !!hashedPassword
 
-const mckDB = { password: {} }
-
-
-const saveNewPassword = (password) => {
-    mckDB.hash = bcrypt.hashSync(password, 10)
-    console.log('senha salva'); s
+    } catch (err) {
+        console.error("Error connect to the datebase:", err);
+        process.exit(1)
+    }
 }
 
-const compareHashPassword = async (password) => {
-    await bcrypt.compare(password, mckDB.hash)
+await main()
+if (!hasPasswords) await promptNewPassword()
+else await promptOldPassword()
+
+
+const saveNewPassword = async (password) => {
+    const hash = await bcrypt.hash(password, 10)
+    await authCollection.insertOne({ type: "auth", hash })
+    hasPasswords = true
+    console.log('Password has been saved!')
+}
+
+const compareHashedPassword = async (password) => {
+    const authData = await authCollection.findOne({ type: "auth" })
+    if (!authData?.hash) {
+        return false
+    }
+
+    return bcrypt.compare(password, authData.hash)
 }
 
 const promptNewPassword = async () => {
-    const response = ('digite uma senha: ')
-    return saveNewPassword(response)
+    const response = prompt('Enter a new password: ')
+    await saveNewPassword(response)
+    await showMenu()
 }
 
 
-const promptPassword = async () => {
+const promptOldPassword = async () => {
     let verifield = false
     while (!verifield) {
-        const response = ('digite sua senha: ')
-        const result = await compareHashPassword(response)
+        const response = prompt('Enter password: ')
+        const result = await compareHashedPassword(response)
         if (result) {
-            console.log('password verifield');
+            console.log('Password verified!');
             verifield = true
-            showMenu()
+            await showMenu()
         } else {
-            console.log('senha incorreta');
+            console.log('Incorrect password.');
 
         }
     }
 }
+
+
+const showMenu = async () => {
+    console.log(`
+1. View passwords
+2. Manage new password
+3. Verify password
+4. Exit`);
+
+    const response = prompt(">")
+    switch (response) {
+        case "1":
+            await viewPasswords();
+            break;
+        case "2":
+            await promptManageNewPassword()
+            break;
+        case "3":
+            await promptOldPassword()
+            break
+        case "4":
+            process.exit()
+        default:
+            console.log("That's an invalid response.");
+            await showMenu()
+
+    }
+
+};
+
+
+
+const viewPasswords = async () => {
+    const passwords = await passwordsCollection.find({}).toArray()
+
+    passwords.forEach(({ source, password }, index) => {
+        console.log(`${index + 1}. ${source} => ${password}`)
+    })
+
+    await showMenu()
+
+}
+
+
+const promptManageNewPassword = async () => {
+    const source = prompt("Enter name for password: ")
+    const password = prompt("Enter password to save: ")
+    await passwordsCollection.findOneAndUpdate(
+        { source },
+        {
+            $set: { password }
+        },
+        {
+            returnDocument: "after",
+            upsert: true
+        }
+    )
+    console.log(`Password for ${source} has been saved!`);
+    await showMenu()
+};
+
+
+
+
+
