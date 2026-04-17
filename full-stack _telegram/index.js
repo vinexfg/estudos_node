29ddca930baae2bf5a54edbfdbff4a58213e94a2
vinexfg <vinysilvaa20@gmail.com>
2026-04-17 14:13:49 -0300

    a

---
+++ full-stack _telegram/index.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+// console.log(new Date().getTime());
+// console.log(new Date(0));
+
+// console.log(new Date(2026, 6, 3, 14, 30, 12));
+
+
+
+let date = new Date("2024-07-02T14:30:00")
+
+// console.log(date.getDate().toString().padStart(2, "0"));
+
+
+
+
+// console.log(date.getDate().toString().padStart(2, "0"));
+
+let month = (date.getMonth() + 1).toString().padStart(2, "0")
+
+let day = date.getDay().toString().padStart(2, "0")
+
+
+
+let year = date.getFullYear()
+let hour = date.getHours()
+let minute = date.getMinutes()
+
+
+console.log(`${day}/ ${month}/ ${minute} às ${hour}:${minute}seg`)
