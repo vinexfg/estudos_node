c9bfc645e9090be97dfee3dcb3c309c2841803f4
vinexfg <vinysilvaa20@gmail.com>
2026-04-17 16:49:13 -0300

    a

--- full-stack _telegram/index.js
+++ full-stack _telegram/index.js
@@ -1,37 +1,55 @@
 
 
 
-
-
-
-
-// console.log(new Date().getTime());
-// console.log(new Date(0));
-
-// console.log(new Date(2026, 6, 3, 14, 30, 12));
-
-
-
 let date = new Date("2024-07-02T14:30:00")
 
-// console.log(date.getDate().toString().padStart(2, "0"));
+
+
+
+
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 // console.log(date.getDate().toString().padStart(2, "0"));
 
-let month = (date.getMonth() + 1).toString().padStart(2, "0")
 
-let day = date.getDay().toString().padStart(2, "0")
 
-let day2 = date.getDay().toString().padStart('2', "0")
-let years = date.getFullYear().toString().padStart('4', "0")
-let year = date.getFullYear()
-let hour = date.getHours()
-let minute = date.getMinutes()
+
+// console.log(date.getDate().toString().padStart(2, "0"));
+
+// let month = (date.getMonth() + 1).toString().padStart(2, "0")
+
+// let day = date.getDay().toString().padStart(2, "0")
+
+// let day2 = date.getDay().toString().padStart('2', "0")
+// let years = date.getFullYear().toString().padStart('4', "0")
+// let year = date.getFullYear()
+// let hour = date.getHours()
+// let minute = date.getMinutes()
 
 
 // console.log(`${day}/ ${month}/ ${minute} às ${hour}:${minute}seg`)
 
-console.log(years);
+// console.log(years);
+
+
+
