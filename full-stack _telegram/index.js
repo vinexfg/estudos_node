a2f52f47118fae6a83d150fd5be9febbc2a466ef
vinexfg <vinysilvaa20@gmail.com>
2026-04-18 12:21:26 -0300

    a

--- full-stack _telegram/index.js
+++ full-stack _telegram/index.js
@@ -1,14 +1,20 @@
 
-class Animal {
-    constructor(name) {
-        this.name = name
-    }
 
-    makeNoise() {
-        console.log("algum som generico do animal");
+
+
+
+let obj = []
+
+
+try {
+    if (!obj.includes(17)) {
+        throw new Error(' o numero 17 nao esta disponivel')
+    }
+} catch (err) {
+    if (err instanceof TypeError) {
+        console.log("erro ao executar a funcao");
 
     }
+    console.error(err);
+
 }
-
-
-
