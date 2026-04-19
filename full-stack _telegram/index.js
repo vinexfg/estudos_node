afc4a3e6bcbbe585d8ba47bfb94199aa21d9cc8e
vinexfg <vinysilvaa20@gmail.com>
2026-04-19 11:30:04 -0300

    a

--- full-stack _telegram/index.js
+++ full-stack _telegram/index.js
@@ -1,20 +1,59 @@
 
 
 
+// let obj = []
 
 
-let obj = []
+
+// try {
+//     if (!obj.includes(17)) {
+//         throw new Error("o numero 17 nao se encontra no obj")
+//     }
+// } catch (err) {
+//     if (err instanceof TypeError) {
+//         console.log('metodo indiponivel');
+
+//     }
+// }
+
+
+
+// let obj = []
+
+
+// try {
+//     if (!obj.includes(17)) {
+//         throw new Error(' o numero 17 nao esta disponivel')
+//     }
+// } catch (err) {
+//     if (err instanceof TypeError) {
+//         console.log("erro ao executar a funcao");
+
+//     }
+//     console.error(err);
+
+// }
+
+
+
+
+
+class MyCustomError {
+    constructor(message) {
+        this.message = "CLASSE DE ERRO CUSTOMIZADDA:" + message
+    }
+}
+
 
 
 try {
-    if (!obj.includes(17)) {
-        throw new Error(' o numero 17 nao esta disponivel')
-    }
+    throw new MyCustomError("erro personalizado")
 } catch (err) {
-    if (err instanceof TypeError) {
-        console.log("erro ao executar a funcao");
+    if (err instanceof MyCustomError) {
+        console.log(err.message);
+
+    } else {
+        console.log('nao foi possivel executar! ');
 
     }
-    console.error(err);
-
 }
