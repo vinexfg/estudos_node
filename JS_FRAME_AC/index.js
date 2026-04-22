61503e9a28dcf61a4c765d0fc5572340f5187289
vinexfg <vinysilvaa20@gmail.com>
2026-04-22 13:45:52 -0300

    a

--- JS_FRAME_AC/index.js
+++ JS_FRAME_AC/index.js
@@ -3,6 +3,18 @@
 
 
 
+const words = ['JavaScript, "html', "css, 'Web"]
 
 
 
+
+const result = words.filter((word) => word.length > 3)
+console.log(result);
+
+
+
+const product = [
+    {
+        description: "teclado", price: 100
+    }
+]
