aabe0fa9d3e56887bc8ea84a068b6e7bd95b749d
Vinicius Silva Gonçalves de Almeida <136178252+vinexfg@users.noreply.github.com>
2026-04-24 17:13:22 -0300

    Update index.js

--- JS_FRAME_AC/index.js
+++ JS_FRAME_AC/index.js
@@ -3,34 +3,41 @@
 
 
 
-const words = ['JavaScript, "html', "css, 'Web"]
+// const words = ["JavaScript", "html", "css", "Web"]
 
 
 
 
-const result = words.filter((word) => word.length > 3)
-console.log(result);
+// const result = words.filter((word)=> word.length > 3)
+// console.log(result);
+
+
+// const products = [
+//     {
+//         description: "teclado", price: 100, promotion: true
+//     },
+//     {
+//         description: "mouse", price: 70, promotion: false
+//     },
+//     {
+//         description: "Monitor", price: 900, promotion: true
+//     }
+// ]
+
+// const promotion = products.filter((product)=> product.promotion === true)
+
+// console.log(promotion);
 
 
 
-const products = [
-    {
-        description: "teclado", price: 100, promotion = true
-    },
-    {
-        description: "mouse", price: 70, promotion = false
-    },
-    {
-        description: "Monitor", price: 900, promotion = true
-    }
-]
+
+// const values = [4,6,8, 12]
 
 
-const promotion = products.filter((product) => product === true)
+const values = [4, 6, 8, 12];
+console.log(values.findIndex((value)=> values > 4));
+
+console.log(values.findIndex((value) => value > 4));
 
 
-console.log(promotion); 
-
-
-
-const numbers = []
+// console.log(values.findIndex((value)=>  value > 4 ));
