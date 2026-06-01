0f6457d398c46c63045951c0d87715f9878e43fd
Vinicius Silva Gonçalves de Almeida <136178252+vinexfg@users.noreply.github.com>
2026-06-01 20:27:43 -0300

    q

--- python/app.js
+++ python/app.js
@@ -2,8 +2,3 @@ function CriarDados() {
   let numeros = [1, 2, 3, 4, 5];
   let soma = numeros.reduce((a, b) => a + b);
 }
-
-function CriarDados() {
-  let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let soma = numeros.reduce((a, b) => a + b);
-}
