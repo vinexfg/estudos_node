c8a9c5eb7d94e3141a80ed43223e845d3ca70bc7
Vinicius Silva Gonçalves de Almeida <136178252+vinexfg@users.noreply.github.com>
2026-06-28 18:25:11 -0300

    a

--- praticando.ts
+++ praticando.ts
@@ -1,3 +1,15 @@
-const numeros = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9];
+const pessoas = [
+  { nome: "vinicus", idade: 62 },
+  { nome: "vitor", idade: 34 },
+  { nome: "carlos", idade: 20 },
+  { nome: "vini", idade: 24 },
+  { nome: "carlinhos", idade: 25 },
+];
 
-const numerosEmDobro = numeros.map((valor: number) => valor * 2);
+const nomes = pessoas.map((obj) => {
+  return obj.nome;
+});
+const idades = pessoas.map(function(obj) => {
+    return obj.idade
+})
+console.log(nomes);
